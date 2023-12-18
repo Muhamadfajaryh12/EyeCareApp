c7e3928d24cc24f0b5ecb0d7798a3efae13235ec
Hilmy Syafiq Arrosyid <142724678+hilmyarrosid@users.noreply.github.com>
2023-12-19 06:29:15 +0700

    Update HypermopiaOrHymopiaContent.kt
    add checkbox

--- app/src/main/java/com/example/eyecareapp/ui/components/content/HypermopiaOrHymopiaContent.kt
+++ app/src/main/java/com/example/eyecareapp/ui/components/content/HypermopiaOrHymopiaContent.kt
@@ -3,217 +3,147 @@ package com.example.eyecareapp.ui.components.content
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.eyecareapp.R
 import com.example.eyecareapp.ui.theme.EyeCareAppTheme
 
-
 @Composable
-fun HypermopiaOrHymopiatestContent(
-    navigateToResult : () -> Unit
+fun ColourBlindContent(
+    navigateToResult: () -> Unit
 ) {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.ArrowBack,
-                    contentDescription = "Back",
-                    modifier = Modifier
-                        .clickable { /* Handle back action */ }
-                )
-                Text(
-                    text = "Vision Test",
-                    style = TextStyle(
-                        fontSize = 20.sp,
-                        color = Color.Black
-                    ),
-                    modifier = Modifier.padding(8.dp)
-                )
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Image(
-                painter = painterResource(id = R.drawable.hypermopiaorhypomiatest), // Ganti dengan gambar yang sesuai
-                contentDescription = "Hypermopia / Hymopiatest Image",
-                modifier = Modifier
-                    .height(200.dp)
-                    .fillMaxWidth()
-                    .background(Color.White)
-            )
-
-            Spacer(modifier = Modifier.padding(10.dp))
-
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .background(
-                        color = Color(0xFF4682A9),
-                        shape = RoundedCornerShape(5.dp)
-                    )
-            ) {
-                Column (
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxSize()
-                ){
-                    AnswerOptionItem(
-                        option1 = "Option 1",
-                        option2 = "Option 2",
-                        option3 = "Option 3",
-                        option4 = "Option 4"
-                    )
-                    Button(onClick = { navigateToResult()},
-                        modifier=Modifier
-                            .width(300.dp)
-                            .clip(shape= RoundedCornerShape(5.dp))
-                    ) {
-                        Text(text = "Selesai")
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun AnswerOptionItem(option1: String, option2: String, option3: String, option4: String) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-
         Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Back",
+                modifier = Modifier
+                    .clickable { /* Handle back action */ }
+            )
+            Text(
+                text = "Hypermopia Or Hymopia Test",
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.hypermopiaorhypomiatest),
+            contentDescription = "Hypermopia Or Hymopia Test ",
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .background(Color.White),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.padding(10.dp))
+        Column(
             modifier = Modifier
                 .background(
                     color = Color(0xFF4682A9),
                     shape = RoundedCornerShape(5.dp)
                 )
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .height(100.dp)
-                    .clip(MaterialTheme.shapes.medium)
-                    .background(Color.White)
-                    .padding(8.dp)
-            ) {// bisa diubah pakai lazygrid
-                Image(
-                    painter = painterResource(id = R.drawable.hypermopiaorhypomiatest), // Ganti dengan gambar yang sesuai
-                    contentDescription = "Option 1",
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
-            }
-
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .height(100.dp)
-                    .clip(MaterialTheme.shapes.medium)
-                    .background(Color.White)
-                    .padding(8.dp)
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.hypermopiaorhypomiatest), // Ganti dengan gambar yang sesuai
-                    contentDescription = "Option 2",
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
+                items(answerOptions) { option ->
+                    AnswerOptionItem(option = option)
+                }
             }
-        }
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Box(
+            Spacer(modifier = Modifier.padding(10.dp))
+            Button(
+                onClick = { navigateToResult() },
                 modifier = Modifier
-                    .weight(1f)
-                    .height(100.dp)
-                    .clip(MaterialTheme.shapes.medium)
-                    .background(Color.White)
-                    .padding(8.dp)
+                    .width(300.dp)
+                    .clip(shape = RoundedCornerShape(5.dp))
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.hypermopiaorhypomiatest), // Ganti dengan gambar yang sesuai
-                    contentDescription = "Option 3",
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
-            }
-
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .height(100.dp)
-                    .clip(MaterialTheme.shapes.medium)
-                    .background(Color.White)
-                    .padding(8.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.hypermopiaorhypomiatest), // Ganti dengan gambar yang sesuai
-                    contentDescription = "Option 4",
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
+                Text(text = "Selesai")
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun PreviewHypermopiaorHymopiatestScreen() {
-    EyeCareAppTheme {
-        HypermopiaOrHymopiatestContent(
-             navigateToResult = {}
+fun AnswerOptionItem(option: Int) {
+    Spacer(modifier = Modifier.padding(10.dp))
+    Row(
+        modifier = Modifier
+            .widthIn(max = 350.dp)
+            .clip(shape = RoundedCornerShape(5.dp))
+            .background(Color.White)
+            .padding(horizontal = 50.dp, vertical = 5.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Checkbox untuk customize
+        var isChecked by remember { mutableStateOf(false) }
+
+        Checkbox(
+            checked = isChecked,
+            onCheckedChange = { isChecked = it },
+            modifier = Modifier.padding(start = 8.dp, top = 8.dp)
+        )
+
+        Spacer(modifier = Modifier.width(16.dp)) // Adjust the width as needed
+
+        Image(
+            painter = painterResource(id = option),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(RoundedCornerShape(5.dp))
         )
     }
 }
+
+val answerOptions = listOf(
+    R.drawable.hypermopiaorhypomiatest,
+    R.drawable.hypermopiaorhypomiatest,
+    R.drawable.hypermopiaorhypomiatest,
+    R.drawable.hypermopiaorhypomiatest
+)
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewColourBlindTestScreen() {
+    EyeCareAppTheme {
+        ColourBlindContent(navigateToResult = {})
+    }
+}
+
+
